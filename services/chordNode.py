254f7324a2e2519bc3438c9e3884afe497eb7206
IsisAmaya <icamayaa@eafit.edu.co>
2024-09-01 03:01:58 -0500

    default time

--- services/chordNode.py
+++ services/chordNode.py
@@ -182,7 +182,7 @@ class ChordNode:
             self.stabilize()
             self.fix_fingers()
             print("Server running at", f"{self.ip}:{self.port}")
-            time.sleep(1)
+            time.sleep(5)
 
             
     def stop_server(self):
